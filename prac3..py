25513a2ccbb9552a887280a790279e22c12279d6
Jinrong Wu <jc482661@ad.jcu.edu.au>
2018-11-27 15:45:48 +0800

    Initial commit

---
+++ prac3..py
@@ -0,0 +1,10 @@
+finished = False
+result = 0
+while not finished:
+    try:
+        # TODO: if any of 'normal' counts are zero, retuen False
+        # TODO: if special characters are required then check the count of those
+        pass
+    except:  # TODO - count each kind of character
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
